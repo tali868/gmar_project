6d520600c60c34c35f669c3a23a6e90f9eaca852
tali <tali.lisov@gmail.com>
2023-05-03 11:32:46 +0300

    run batch of 16 in size

--- create_vessel_train_set.py
+++ create_vessel_train_set.py
@@ -1,7 +1,35 @@
 import json
 
+import torch
+from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
 import pandas as pd
+
+from encoder import VESSEL_ID, PREV_PORT
+
+
+class VesselPortDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        input_vessel_id = self.data.iloc[index][f"input_{VESSEL_ID}"]
+        input_port_id = self.data.iloc[index][f"input_{PREV_PORT}"]
+        output_label = self.data.iloc[index]["output_label"]
+
+        return (
+            (torch.tensor(input_vessel_id, dtype=torch.long), torch.tensor(input_port_id, dtype=torch.long)),
+            torch.tensor(output_label, dtype=torch.long)
+        )
+
+
+def dataloader_from_df(data: pd.DataFrame, batch_size: int):
+    dataset = VesselPortDataset(data)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    return dataloader
 
 
 def load_dataset():
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-from create_vessel_train_set import load_dataset, split_datasets
+from create_vessel_train_set import load_dataset, split_datasets, dataloader_from_df
 from encoder import add_ordinal_label, VESSEL_ID, PREV_PORT, PORT
 
 
@@ -15,7 +15,7 @@ class PortPredictNeuralNetwork(nn.Module):
     def __init__(self, vessel_dim, port_dim, embedding_dim, hidden_dim, output_dim):
         super(PortPredictNeuralNetwork, self).__init__()
         self.vessel_embedding = nn.Embedding(vessel_dim, embedding_dim)
-        self.port_embedding = nn.Embedding(port_dim, embedding_dim)        # This is our holy embedding layer
+        self.port_embedding = nn.Embedding(port_dim, embedding_dim)  # This is our holy embedding layer
 
         # These are two fully connected layers
         self.layer_1 = nn.Linear(embedding_dim * 2, hidden_dim)
@@ -25,35 +25,31 @@ class PortPredictNeuralNetwork(nn.Module):
         # Pass input through the embedding layer
         vessel_embed = self.vessel_embedding(inputs[0])
         port_embed = self.port_embedding(inputs[1])
-        embed = torch.cat((vessel_embed, port_embed), 1)
+        embed = torch.cat((vessel_embed, port_embed), 2)
 
         # Pass input through the hidden layer
         out = F.relu(self.layer_1(embed))
         # Pass input through the final linear layer
         out = self.layer_2(out)
-        out = F.log_softmax(out, 1)
+        out = F.log_softmax(out, 2)
         return out
 
 
-def train_epoch(train_df, val_df):
+def train_epoch(train_dl, val_dl):
     total_loss = 0
-    accurate = 0
-    for index, row in train_df[[VESSEL_ID, PREV_PORT, f"input_{VESSEL_ID}", f"input_{PREV_PORT}", "output_label"]].iterrows():
-        # Every data instance is an input + label pair
-        inputs = (torch.tensor(row[f"input_{VESSEL_ID}"], dtype=torch.long), torch.tensor(row[f"input_{PREV_PORT}"], dtype=torch.long))
-
+    accuracy = []
+    for inputs, target in train_dl:
         # Zero your gradients for every batch!
         model.zero_grad()
 
-        # Run the forward pass, getting softmax probabilities
+        # Run the  forward pass, getting softmax probabilities
         output = model(inputs)
-        predicted_ix = output.argmax().item()
-        target = torch.tensor(row["output_label"], dtype=torch.long)
-        if target.item() == predicted_ix:
-            accurate += 1
+        batch_sum = torch.sum(output.argmax(2) == target).item()
+        batch_len = inputs[0].size()[0]
+        accuracy.append(batch_sum / batch_len)
 
         # Step 4. Compute your loss function
-        loss = loss_function(output, target)
+        loss = loss_function(output.squeeze(), target.squeeze())
 
         # Step 5. Do the backward pass and update the gradient
         loss.backward()
@@ -64,8 +60,8 @@ def train_epoch(train_df, val_df):
         # Get the Python number from a 1-element Tensor by calling tensor.item()
         total_loss += loss.item()
 
-    val_err = run_validation(val_df)
-    return total_loss, accurate / (train_df.shape[0]), val_err
+    # val_err = run_validation(val_dl)
+    return total_loss, sum(accuracy) / len(accuracy), 0
 
 
 def run_validation(val_df):
@@ -104,6 +100,9 @@ train_set_df = train_set_df.sample(frac=1).reset_index(drop=True)
 val_set_df = val_set_df.sample(frac=1).reset_index(drop=True)
 test_set_df = test_set_df.sample(frac=1).reset_index(drop=True)
 
+train_dataloader = dataloader_from_df(train_set_df, 16)
+validation_dataloader = dataloader_from_df(val_set_df, 100)
+
 # Initialize the model
 vessels_size = len(unique_vessels)
 ports_size = len(unique_ports)
@@ -117,8 +116,8 @@ optimizer = optim.SGD(model.parameters(), lr=0.005)
 
 loss_function = nn.NLLLoss()
 
-for epoch in range(100):
-    curr_loss, train_accuracy, val_accuracy = train_epoch(train_set_df, val_set_df)
+for epoch in range(800):
+    curr_loss, train_accuracy, val_accuracy = train_epoch(train_dataloader, validation_dataloader)
     print(f"Epoc: {epoch}; Loss: {round(curr_loss, 3)}; Train Accuracy: {round(train_accuracy*100, 2)}%; "
           f"Validation Accuracy: {round(val_accuracy*100, 2)}%")
 # for epoch, loss, accuracy in losses:
